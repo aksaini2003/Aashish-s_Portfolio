784f771e692c71fcc8193509fb396423a101c30d
AK47SAINI <aksaini2282003@gmail.com>
2026-01-12 23:10:35 +0530

    updated_next.config_file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
+const basePath = "";
+
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath,
+
   images: {
     unoptimized: true,
   },
